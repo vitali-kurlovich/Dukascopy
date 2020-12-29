a17c082132412119fc2c8c7e4f08a55b3b439647
Vitali Kurlovich <vitalikurlovich@gmail.com>
2020-12-29 16:24:38 +0300

    update linux tests

--- Tests/DukascopyTests/XCTestManifests.swift
+++ Tests/DukascopyTests/XCTestManifests.swift
@@ -3,8 +3,6 @@ import XCTest
 #if !canImport(ObjectiveC)
     public func allTests() -> [XCTestCaseEntry] {
         [
-            testCase(DukascopyTests.allTests),
-            testCase(DukascopyURLFactoryTests.allTests),
             testCase(TicksCollectionTest.allTests),
             testCase(DukascopyCandlesCollectionTest.allTests),
 
